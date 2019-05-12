6011206e00083ccea4ebd83875f7a7d8b4809b65
Kshitij Sharma <ksharma377@gmail.com>
2019-05-12 23:10:22 +0530

    Added java version

--- Algorithms/Z Algorithm/ZAlgorithm.cpp
+++ Algorithms/Z Algorithm/ZAlgorithm.cpp
@@ -2,42 +2,42 @@
 using namespace std;
 
 void ZAlgorithm(string& s) {
-  int n = s.length();
-  int z[n];
-  z[0] = 0;
-  int l = 0, r = 0;
-  for (int i = 1; i < n; i++) {
-    if (i > r) {
-      l = r = i;
-      while (r < n && s[r] == s[r - l]) {
-        r++;
-      }
-      z[i] = r - l;
-      r--;
-    } else {
-      int k = i - l;
-      if (z[k] < r - i + 1) {
-        z[i] = z[k];
-      } else {
-        l = i;
-        while (r < n && s[r] == s[r - l]) {
-          r++;
+    int n = s.length();
+    int z[n];
+    z[0] = 0;
+    int l = 0, r = 0;
+    for (int i = 1; i < n; i++) {
+        if (i > r) {
+            l = r = i;
+            while (r < n && s[r] == s[r - l]) {
+                r++;
+            }
+            z[i] = r - l;
+            r--;
+        } else {
+            int k = i - l;
+            if (z[k] < r - i + 1) {
+                z[i] = z[k];
+            } else {
+                l = i;
+                while (r < n && s[r] == s[r - l]) {
+                    r++;
+                }
+                z[i] = r - l;
+                r--;
+            }
         }
-        z[i] = r - l;
-        r--;
-      }
     }
-  }
-  for (int i = 0; i < n; i++) {
-    cout << z[i] << ' ';
-  }
+    for (int i = 0; i < n; i++) {
+        cout << z[i] << ' ';
+    }
 }
 
 int main() {
-  string txt, pat;
-  cin >> txt >> pat;
-  string s = pat + "$" + txt;
-  cout << s << endl;
-  ZAlgorithm(s);
-  return 0;
+    string txt, pat;
+    cin >> txt >> pat;
+    string s = pat + "$" + txt;
+    cout << s << endl;
+    ZAlgorithm(s);
+    return 0;
 }
---
+++ Algorithms/Z Algorithm/ZAlgorithm.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+class ZAlgorithmExecutor {
+    private void ZAlgorithm(String s) {
+        int n = s.length();
+        int[] z = new int[n];
+        z[0] = 0;
+        int start = 0, end = 0;
+        for (int i = 1; i < n; i++) {
+            if (i > end) {
+                start = end = i;
+                while (end < n && s.charAt(end) == s.charAt(end - start)) {
+                    end++;
+                }
+                z[i] = end - start;
+                end--;
+            } else {
+                int k = i - start;
+                if (z[k] < end - i + 1) {
+                    z[i] = z[k];
+                } else {
+                    start = i;
+                    while (end < n && s.charAt(end) == s.charAt(end - start)) {
+                        end++;
+                    }
+                    z[i] = end - start;
+                    end--;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(z[i] + " ");
+        }
+    }
+
+    public void run() {
+        final Scanner scanner = new Scanner(System.in);
+        String text = scanner.next();
+        String pattern = scanner.next();
+        String s = pattern + "$" + text;
+        System.out.println(s);
+        ZAlgorithm(s);
+    }
+}
+
+public class ZAlgorithm {
+    public static void main(String[] args) {
+        new ZAlgorithmExecutor().run();
+    }
+}
