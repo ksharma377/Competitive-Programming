64d31b6f0939cfeb10fb653b0393480c1fa43ec2
Kshitij Sharma <ksharma377@gmail.com>
2019-05-12 13:36:15 +0530

    Changed tab size to 4

--- Data Structures/Fenwick Tree/FenwickTree.cpp
+++ Data Structures/Fenwick Tree/FenwickTree.cpp
@@ -3,66 +3,66 @@ using namespace std;
 
 class FenwickTree {
 private:
-  vector<int> fen;
-  int n;
+    vector<int> fen;
+    int n;
 
 public:
-  FenwickTree(int _n) {
-    n = _n + 1;
-    fen.resize(n);
-  }
-
-  void build(const vector<int>& arr, int n) {
-    for (int i = 1; i <= n; i++) {
-      update(i, arr[i]);
+    FenwickTree(int _n) {
+        n = _n + 1;
+        fen.resize(n);
     }
-  }
 
-  void update(int i, int delta) {
-    for (; i <= n; i += i & -i) {
-      fen[i] += delta;
+    void build(const vector<int>& arr, int n) {
+        for (int i = 1; i <= n; i++) {
+            update(i, arr[i]);
+        }
     }
-  }
 
-  int query(int i) {
-    int sum = 0;
-    for (; i > 0; i -= i & -i) {
-      sum += fen[i];
+    void update(int i, int delta) {
+        for (; i <= n; i += i & -i) {
+            fen[i] += delta;
+        }
     }
-    return sum;
-  }
 
-  int query(int l, int r) {
-    return query(r) - query(l - 1);
-  }
+    int query(int i) {
+        int sum = 0;
+        for (; i > 0; i -= i & -i) {
+            sum += fen[i];
+        }
+        return sum;
+    }
+
+    int query(int l, int r) {
+        return query(r) - query(l - 1);
+    }
 };
 
 int main() {
-  int n;
-  scanf("%d", &n);
-  vector<int> a(n + 1);
-  for (int i = 1; i <= n; i++) {
-    scanf("%d", &a[i]);
-  }
-  FenwickTree tree(n);
-  tree.build(a, n);
-  int queries;
-  scanf("%d", &queries);
-  while (queries-- > 0) {
-    char type[10];
-    scanf("%s", &type);
-    if (type[0] == 'u') {
-      int idx, val;
-      scanf("%d %d", &idx, &val);
-      int delta = val - a[idx];
-      a[idx] = val;
-      tree.update(idx, delta);
-    } else {
-      int l, r;
-      scanf("%d %d", &l, &r);
-      int ans = tree.query(l, r);
-      printf("%d\n", ans);
+    int n;
+    scanf("%d", &n);
+    vector<int> a(n + 1);
+    for (int i = 1; i <= n; i++) {
+        scanf("%d", &a[i]);
     }
-  }
-  return 0;
+    FenwickTree tree(n);
+    tree.build(a, n);
+    int queries;
+    scanf("%d", &queries);
+    while (queries-- > 0) {
+        char type[10];
+        scanf("%s", &type);
+        if (type[0] == 'u') {
+            int idx, val;
+            scanf("%d %d", &idx, &val);
+            int delta = val - a[idx];
+            a[idx] = val;
+            tree.update(idx, delta);
+        } else {
+            int l, r;
+            scanf("%d %d", &l, &r);
+            int ans = tree.query(l, r);
+            printf("%d\n", ans);
+        }
+    }
+    return 0;
 }
